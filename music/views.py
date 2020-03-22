fd91cc89291ce823e01c8884953f496b36f7893c
katerebello <61910583+katerebello@users.noreply.github.com>
2020-03-22 11:56:42 +0530

    modified

--- music/views.py
+++ music/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from .models import Album
+"""from django.http import Http404"""
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
     all_albums = Album.objects.all()
-    # return render(request(request, "music/index.html", {'all_albums': all_albums}))A
-    template = loader.get_template('music/index.html')
-    context = {
-    	'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album id:" + str(album_id) + "<h2>")
+    """try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")"""
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
 
 
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import include, url
 from django.contrib import admin
+
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
 ]
+
